eb9558819288a2057a9959f3d558b81cc75c190f
7e1ght <7e1ght.jm@gmail.com>
2019-10-20 12:43:21 +0300

    new

---
+++ Assets/Scripts/levelMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelMenu : MonoBehaviour
+{   
+    public void levelOne()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void levelTwo()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void levelThree()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void backToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
